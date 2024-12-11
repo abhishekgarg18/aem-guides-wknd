e400e8b4bd757ef10030788c316c4d79a7ff9d5d
Abhishek Garg <abhigarg@adobe.com>
2024-12-11 10:28:52 +0530

    updating my component

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/MyComponent.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/MyComponent.java
@@ -9,6 +9,34 @@ public class MyComponent implements MyService {
 
     @Override
     public void execute() {
+        String role = "admin";
+        String message = getWelcomeMessage(role);
+        System.out.println(message);
+        printMultiLineString() ;
+    }
 
+    public static String getWelcomeMessage(String role) {
+        String message;
+        switch (role) {
+            case "admin":
+                message = "Welcome, Administrator!";
+                break;
+            case "user":
+                message = "Welcome, User!";
+                break;
+            case "guest":
+                message = "Welcome, Guest!";
+                break;
+            default:
+                message = "Welcome!";
+        }
+        return message;
+    }
+
+    public static void printMultiLineString() {
+        String text = "This is a multi-line\n"
+                + "string example\n"
+                + "that could use modern features.";
+        System.out.println(text);
     }
 }
